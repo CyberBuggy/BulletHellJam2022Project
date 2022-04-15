57575e2752bad4f5f835269c5a9f65f9e15289df
KaitoMajima <kaitomajima@outlook.com>
2022-04-15 16:49:14 -0300

    Setup SuperUnityBuild

---
+++ Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,38 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Debug,
+        Release,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x86,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637856379240965808);
+    public const string version = "1.0.0.1";
+    public const ReleaseType releaseType = ReleaseType.Debug;
+    public const Platform platform = Platform.PC;
+    public const Architecture architecture = Architecture.Windows_x86;
+    public const Distribution distribution = Distribution.None;
+}
+
